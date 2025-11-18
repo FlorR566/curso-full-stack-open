b26e7da6fc7fcb57a513e45d50a15142e07ae3c6
FlorR566 <florodriguez566@gmail.com>
2025-11-18 10:47:32 -0300

    Ejemplo de re-renderizado en React

---
+++ part1/courseinfo/src/App_renderizado.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const App = () => {
+	const [counter, setCounter] = useState(0);
+
+	const increaseByOne = () => setCounter(counter + 1);
+	const setToZero = () => setCounter(0);
+
+	// setTimeout(() => setCounter(counter + 1), 1000);
+	// console.log("rendering ...", counter);
+
+	// const handleClick = () => {
+	// 	console.log("clicked");
+	// };
+	return (
+		<div>
+			<div>{counter}</div>
+			<button onClick={increaseByOne}>plus</button>
+			<button onClick={setToZero}>zero</button>
+		</div>
+	);
+};
+
+export default App;
