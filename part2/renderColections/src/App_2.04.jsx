4c74a5b4f1f7a90b3ed7c4b0e48f2b1e01a9dc5b
FlorR566 <florodriguez566@gmail.com>
2025-11-21 18:47:40 -0300

    Arreglar error de copiado en archivo base App

--- part2/renderColections/src/App_2.04.jsx
+++ part2/renderColections/src/App_2.04.jsx
@@ -1,51 +1,38 @@
-const Header = ({ course }) => {
-	//console.log(course);
-	return <h1>{course}</h1>;
-};
+import Course from "./components/Course_2.02";
 
-const Part = ({ part }) => {
-	//console.log("-->", part);
+const App = () => {
+	const course = {
+		id: 1,
+		name: "Half Stack application development",
+		parts: [
+			{
+				name: "Fundamentals of React",
+				exercises: 10,
+				id: 1,
+			},
+			{
+				name: "Using props to pass data",
+				exercises: 7,
+				id: 2,
+			},
+			{
+				name: "State of a component",
+				exercises: 14,
+				id: 3,
+			},
+			{
+				name: "Redux",
+				exercises: 11,
+				id: 4,
+			},
+		],
+	};
+
 	return (
-		<p>
-			{part.name}: {part.exercises}
-		</p>
+		<>
+			<Course course={course} />
+		</>
 	);
 };
 
-const Content = ({ parts }) => {
-	const part = parts.map(function (part) {
-		return (
-			<li key={part.id}>
-				<Part part={part} />
-			</li>
-		);
-	});
-
-	return part;
-};
-
-const Total = ({ parts }) => {
-	const totalExercises = parts.reduce(function (sum, p) {
-		// console.log("Sumatoria:", sum);
-		// console.log("valor a sumar:", p.exercises);
-		return sum + p.exercises;
-	}, 0);
-
-	return (
-		<p>
-			<strong>Number of exercises: {totalExercises} </strong>
-		</p>
-	);
-};
-
-const Course = ({ course }) => {
-	return (
-		<div>
-			<Header course={course.name} />
-			<Content parts={course.parts} />
-			<Total parts={course.parts} />
-		</div>
-	);
-};
-
-export default Course;
+export default App;
