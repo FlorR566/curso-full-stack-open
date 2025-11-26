4170351bf83fb1c40b4d2ecca8cbe63da1e35a0a
FlorR566 <florodriguez566@gmail.com>
2025-11-26 09:05:34 -0300

    Agregar funcionalidad para sumar personas al arr person y mostrarlas en forma de lista

--- part2/renderColections/src/App_2.06_agenda.jsx
+++ part2/renderColections/src/App_2.06_agenda.jsx
@@ -1,22 +1,42 @@
 import { useState } from "react";
 
+const Person = ({ persons }) =>
+	persons.map((person) => <li key={person.name}>{person.name}</li>);
+
 const App = () => {
 	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-	const [newName, setNewName] = useState("");
+	const [newName, setNewName] = useState("add new name...");
+
+	const addPerson = (event) => {
+		event.preventDefault();
+		const personObject = {
+			name: newName,
+		};
+
+		setPersons(persons.concat(personObject));
+		setNewName(" ");
+	};
+
+	const handleAddName = (event) => {
+		//console.log(event.target.value);
+		setNewName(event.target.value);
+	};
 
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<form>
+			<form onSubmit={addPerson}>
 				<div>
-					name: <input />
+					name: <input value={newName} onChange={handleAddName} />
 				</div>
 				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			...
+			<ul>
+				<Person persons={persons} />
+			</ul>
 		</div>
 	);
 };
