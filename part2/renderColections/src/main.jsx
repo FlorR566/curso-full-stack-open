f5f9d518fed8a451b2dd744eee6d0afbf77f086e
FlorR566 <florodriguez566@gmail.com>
2025-11-28 16:11:53 -0300

    incorporar el uso de POST para guardar las notas en el servidor db.json

--- part2/renderColections/src/main.jsx
+++ part2/renderColections/src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./App_2.10_agenda";
+import App from "./App";
 import { StrictMode } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
