6d489fa1b2e7beb8c860623648479b7ee085589a
FlorR566 <florodriguez566@gmail.com>
2025-12-01 20:17:24 -0300

    Agregar comunicación con backend a través del uso de GET, PUSH, PUT y usar promesas

--- part2/renderColections/src/App.jsx
+++ part2/renderColections/src/App.jsx
@@ -1,49 +1,48 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+import noteService from "./services/notes";
 import Note from "./components/Note";
 
 const App = () => {
 	const [notes, setNotes] = useState([]);
-	const [newNote, setNewNote] = useState("a new note...");
+	const [newNote, setNewNote] = useState("");
 	const [showAll, setShowAll] = useState(true);
 
 	useEffect(() => {
-		console.log("effect");
-		axios.get("http://localhost:3001/notes").then((response) => {
-			console.log("promise fulfilled");
-			setNotes(response.data);
+		noteService.getAll().then((initialNotes) => {
+			setNotes(initialNotes);
 		});
 	}, []);
-	console.log("render", notes.length, "notes");
 
 	const addNote = (event) => {
 		event.preventDefault();
-		// console.log("button clicked", event.target);
 		const noteObject = {
 			content: newNote,
-			important: Math.random() < 0.5,
+			important: Math.random() > 0.5,
 		};
 
-		axios.post("http://localhost:3001/notes", noteObject).then((response) => {
-			console.log(response);
-			setNotes(notes.concat(response.data));
-			setNewNote(" ");
+		noteService.create(noteObject).then((returnedNote) => {
+			setNotes(notes.concat(returnedNote));
+			setNewNote("");
 		});
-	};
-
-	const handleNoteChange = (event) => {
-		console.log(event.target.value); // valor de entrada de ese momento
-		setNewNote(event.target.value);
 	};
 
 	const toggleImportanceOf = (id) => {
-		const url = `http://localhost:3001/notes/${id}`;
 		const note = notes.find((n) => n.id === id);
 		const changedNote = { ...note, important: !note.important };
 
-		axios.put(url, changedNote).then((response) => {
-			setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
-		});
+		noteService
+			.update(id, changedNote)
+			.then((returnedNote) => {
+				setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+			})
+			.catch((error) => {
+				alert(`the note '${note.content}' was already deleted from server`);
+				setNotes(notes.filter((n) => n.id !== id));
+			});
+	};
+
+	const handleNoteChange = (event) => {
+		setNewNote(event.target.value);
 	};
 
 	const notesToShow = showAll ? notes : notes.filter((note) => note.important);
---
+++ part2/renderColections/src/services/notes.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/notes";
+
+// const getAll = () => {
+// 	const request = axios.get(baseUrl);
+// 	return request.then((response) => response.data);
+// };
+const getAll = () => {
+	const request = axios.get(baseUrl);
+	const nonExisting = {
+		id: 10000,
+		content: "This note is not saved to server",
+		important: true,
+	};
+	return request.then((response) => response.data.concat(nonExisting)); // nonExisting es una nota inventada que no se guardó en el servidor, para probar que funciona la promesa
+};
+
+const create = (newObject) => {
+	const request = axios.post(baseUrl, newObject);
+	return request.then((response) => response.data);
+};
+
+const update = (id, newObject) => {
+	const request = axios.put(`${baseUrl}/${id}`, newObject);
+	return request.then((response) => response.data);
+};
+
+export default {
+	getAll,
+	create,
+	update,
+};
+// Este módulo devuelve un objeto que tiene tres funciones (getAll, create y update), como propiedades que se ocupan de las notas.
+// Las funciones devuelven directamente las promesas devueltas por los métodos axios.
