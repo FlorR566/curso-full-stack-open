bbad39a8d8fc2c44f738ecb88a414327c3c73d70
FlorR566 <florodriguez566@gmail.com>
2025-12-03 19:28:11 -0300

    agregar componente para notificaciones y Footer, estilos con CSS

--- part2/renderColections/src/App.jsx
+++ part2/renderColections/src/App.jsx
@@ -1,11 +1,27 @@
 import { useState, useEffect } from "react";
 import noteService from "./services/notes";
 import Note from "./components/Note";
+import Notification from "./components/Notification";
+
+const Footer = () => {
+	const footerStyle = {
+		color: "green",
+		fontStyle: "italic",
+		fontSize: 16,
+	};
+	return (
+		<div style={footerStyle}>
+			<br />
+			<em>Note app, Department of Computer Science, University of Helsinki</em>
+		</div>
+	);
+};
 
 const App = () => {
 	const [notes, setNotes] = useState([]);
 	const [newNote, setNewNote] = useState("");
 	const [showAll, setShowAll] = useState(true);
+	const [errorMessage, setErrorMessage] = useState(null);
 
 	useEffect(() => {
 		noteService.getAll().then((initialNotes) => {
@@ -38,7 +54,12 @@ const App = () => {
 			})
 			// eslint-disable-next-line no-unused-vars
 			.catch((error) => {
-				alert(`the note '${note.content}' was already deleted from server`);
+				setErrorMessage(
+					`Note '${note.content}' was already removed from server`
+				);
+				setTimeout(() => {
+					setErrorMessage(null);
+				}, 5000);
 				setNotes(notes.filter((n) => n.id !== id));
 			});
 	};
@@ -52,6 +73,7 @@ const App = () => {
 	return (
 		<div>
 			<h1>Notes</h1>
+			<Notification message={errorMessage} />
 			<div>
 				<button onClick={() => setShowAll(!showAll)}>
 					show {showAll ? "important" : "all"}
@@ -70,6 +92,7 @@ const App = () => {
 				<input value={newNote} onChange={handleNoteChange} />
 				<button type="submit">save</button>
 			</form>
+			<Footer />
 		</div>
 	);
 };
