beff6baf32037245e371299c4394933439b38a99
FlorR566 <florodriguez566@gmail.com>
2025-12-03 17:17:01 -0300

    dejar a render list como componente puro, solo renderiza. Dejar en App la lógica para actualizar el estado luego de eliminar una persona.

--- part2/renderColections/src/components/PersonList.jsx
+++ part2/renderColections/src/components/PersonList.jsx
@@ -1,26 +1,14 @@
-import personService from "../services/persons";
-
-const PersonList = ({ persons, updateList }) =>
-	persons.map((person) => {
-		const handleDelete = (id, name, phone) => {
-			confirm(`Delete ${name} ${phone}`)
-				? personService.remove(id).then((itemRemoved) => {
-						updateList();
-						console.log("Eliminado --->", itemRemoved);
-				  })
-				: "";
-		};
-
-		return (
-			<p key={person.id}>
-				{person.name}: {person.phone}
-				<button
-					onClick={() => handleDelete(person.id, person.name, person.phone)}
-				>
-					Delete
-				</button>
-			</p>
-		);
-	});
+const PersonList = ({ persons, onDelete }) => {
+	return (
+		<>
+			{persons.map((person) => (
+				<p key={person.id}>
+					{person.name}: {person.phone}
+					<button onClick={() => onDelete(person.id)}>Delete</button>
+				</p>
+			))}
+		</>
+	);
+};
 
 export default PersonList;
