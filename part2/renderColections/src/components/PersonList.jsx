22a7c3327f81ed2cfeaa0d0ee7ee8687172e4e9d
FlorR566 <florodriguez566@gmail.com>
2025-12-03 08:53:39 -0300

    Agregar un menejador de borrado para el boton Delete

--- part2/renderColections/src/components/PersonList.jsx
+++ part2/renderColections/src/components/PersonList.jsx
@@ -1,8 +1,26 @@
-const PersonList = ({ persons }) =>
-	persons.map((person) => (
-		<p key={person.name}>
-			{person.name}: {person.phone}
-		</p>
-	));
+import personService from "../services/persons";
+
+const PersonList = ({ persons, updateList }) =>
+	persons.map((person) => {
+		const handleDelete = (id, name, phone) => {
+			confirm(`Delete ${name} ${phone}`)
+				? personService.remove(id).then((itemRemoved) => {
+						updateList();
+						console.log("Eliminado --->", itemRemoved);
+				  })
+				: "";
+		};
+
+		return (
+			<p key={person.id}>
+				{person.name}: {person.phone}
+				<button
+					onClick={() => handleDelete(person.id, person.name, person.phone)}
+				>
+					Delete
+				</button>
+			</p>
+		);
+	});
 
 export default PersonList;
