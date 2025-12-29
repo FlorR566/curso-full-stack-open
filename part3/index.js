68aa902d0fae483aa6f29fe1083bbbc70de3e19c
FlorR566 <florodriguez566@gmail.com>
2025-12-29 15:13:29 -0300

    Add endpoint to fetch note by id

--- part3/index.js
+++ part3/index.js
@@ -23,8 +23,14 @@ app.get("/", (request, response) => {
 	response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes", (request, response) => {
-	response.json(notes);
+app.get("/api/notes/:id", (request, response) => {
+	const id = Number(request.params.id);
+	const note = notes.find((note) => {
+		console.log(note.id, typeof note.id, id, typeof id, note.id === id);
+		return note.id === id;
+	});
+	console.log(note);
+	response.json(note);
 });
 
 const PORT = 3001;
