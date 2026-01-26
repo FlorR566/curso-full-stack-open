a7659c83cf7dd54ed0afb46d19d4b08f86e581c3
FlorR566 <florodriguez566@gmail.com>
2026-01-26 16:25:09 -0300

    Reemplace 'App' for 'App_2.16_agenda' to build phonebook frontend

--- part2/renderColections/src/main.jsx
+++ part2/renderColections/src/main.jsx
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App_2.16_agenda";
 import { StrictMode } from "react";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<StrictMode>
 		<App />
-	</StrictMode>
+	</StrictMode>,
 );
